76b8d59131b05de3ea473a399bb3d6a3fc579774
Anish Poudel <anishpoudel@Anishs-MacBook-Air.local>
2024-12-31 10:57:42 +0545

    api response structure updated.

--- src/api_response.rs
+++ src/api_response.rs
@@ -13,13 +13,13 @@ impl JsonResponse {
     pub fn error(err: impl Serialize, message: Option<String>) -> JsonResponse {
         Self::Error(ErrorResponse {
             error: json!(err),
-            message: message.or(Some("An error occured.".to_string())),
+            message: message.unwrap_or("An error occured.".to_string()),
         })
     }
     pub fn data(data: impl Serialize, message: Option<String>) -> JsonResponse {
         Self::Data(DataResponse {
             data: json!(data),
-            message: message.or(Some("Data retrieved successfully".to_string())),
+            message: message.unwrap_or("Data retrieved successfully".to_string()),
         })
     }
     pub fn paginate(
@@ -30,7 +30,7 @@ impl JsonResponse {
         Self::Paginate(PaginatedResponse {
             _metadata: metadata,
             data: json!(data),
-            message: message.or(Some("Data retrieved successfully".to_string())),
+            message: message.unwrap_or("Data retrieved successfully".to_string()),
         })
     }
 }
@@ -38,13 +38,13 @@ impl JsonResponse {
 #[derive(Serialize)]
 pub struct ErrorResponse {
     pub error: Value,
-    pub message: Option<String>,
+    pub message: String,
 }
 
 #[derive(Serialize)]
 pub struct DataResponse {
     pub data: Value,
-    pub message: Option<String>,
+    pub message: String,
 }
 
 #[derive(Debug, Serialize, Default)]
@@ -75,7 +75,7 @@ impl ResponseMetadata {
 pub struct PaginatedResponse {
     pub data: Value,
     pub _metadata: ResponseMetadata,
-    pub message: Option<String>,
+    pub message: String,
 }
 
 // error: [{"title": ["Row not found", "hello"]}, {"email": ["Not valid", ""]}]
